d0fdae73da12a061527c6a53e8164f7ffb4c6f03
Jakooth <ivan.k.vladimirov@gmail.com>
2016-06-02 15:39:16 +0300

    Move Google analytics
    Now page loads way faster with Google analytics at the end.

--- controllers/INIT.js
+++ controllers/INIT.js
@@ -9,16 +9,16 @@ $(document).ready(function() {
 		   .init();
 		   
 	i18next.changeLanguage('bg');
+	
+	/**
+	 * Google Analytics
+	 */
+
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-77376926-1', 'auto');
+	ga('send', 'pageview');
 });
-
-/**
- * Google Analytics
- */
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-77376926-1', 'auto');
-ga('send', 'pageview');
