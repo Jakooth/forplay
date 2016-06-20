dac4aa4ec1b75bccee59247d1481612bafd30c1b
Jakooth <ivan.k.vladimirov@gmail.com>
2016-06-20 14:08:47 +0300

    Include schema.org meta data
    Adding flash cards for review objects.

--- meta.php
+++ meta.php
@@ -35,7 +35,6 @@ if (sizeof($params) < 4) {
         echo '<meta property="og:locale" content="bg_BG">';
     }
 } else {
-    
     $get_article_sql = "SELECT * FROM for_articles 
 					    WHERE article_id = {$params [4]};";
     
@@ -140,6 +139,78 @@ if (sizeof($params) < 4) {
                  '/' . $article['cover_img'] . '&w=1280&h=720">';
         echo '<meta property="og:site_name" content="Forplay">';
         echo '<meta property="og:locale" content="bg_BG">';
+        
+        if ($article['subtype'] == 'review') {
+            $mysqlDate = strtotime($article['date']);
+            $googleDate = date("c", $mysqlDate);
+            $thing_type = 'Thing';
+            $thing_director = '';
+            
+            $thing_mysqlDate = strtotime($article['prime']['date']);
+            $thing_googleDate = date("c", $thing_mysqlDate);
+            
+            if ($article['prime']['object'] == 'movie') {
+                $director_sql = "SELECT for_tags.*, for_rel_relative.related_subtype FROM for_tags
+							     LEFT JOIN for_rel_relative 
+							     ON for_tags.tag_id = for_rel_relative.related_tag_id
+							     WHERE for_rel_relative.tag_id = {$article['prime']['tag_id']}
+							     AND (for_rel_relative.related_subtype = 'director')
+							     GROUP BY tag_id, related_tag_id, related_subtype;";
+                
+                $director_result = mysqli_query($link, $director_sql);
+                
+                if ($director_result) {
+                    while ($tag = mysqli_fetch_assoc($director_result)) {
+                        $article['prime']['director'][] = $tag['en_name'];
+                    }
+                }
+                
+                $thing_type = 'Movie';
+                $thing_director = '"director": "' .
+                         join(', ', $article['prime']['director']) . '",';
+            }
+            
+            if ($article['prime']['object'] == 'game') {
+                $thing_type = 'Game';
+            }
+            
+            echo '<script type="application/ld+json">' . '{
+                "@context": "http://schema.org/",
+                "@type": "Review",
+                "datePublished": "' . $googleDate . '",
+                "description": "' . $article['subtitle'] . '",    
+                "itemReviewed": {
+                    "@type": "' . $thing_type . '",
+                    "name": "' . $article['title'] .
+                     '",
+                    "image": "https://forplay.bg/forapi/phplib/timthumb/timthumb.php?src=/assets/articles/' . substr(
+                            $article['cover_img'], 0, 
+                            strripos($article['cover_img'], '-')) . '/' .
+                     $article['cover_img'] . '&w=1280&h=720",
+                    "dateCreated": "' .
+                     $thing_googleDate . '",
+                    ' . $thing_director . '
+                    "sameAs": "' .
+                     $article['prime']['site'] . '"
+                },
+                "author": {
+                    "@type": "Person",
+                    "name": "' .
+                     join(', ', $article['authors']) . '",
+                    "sameAs": "https://www.forplay.bg"
+                },
+                "reviewRating": {
+                    "@type": "Rating",
+                    "ratingValue": "' .
+                     round($article['hype'] / 10, 0, PHP_ROUND_HALF_UP) . '",
+                    "bestRating": "10"
+                },
+                "publisher": {
+                    "@type": "Organization",
+                    "name": "Forplay"
+                }
+            }' . '</script>';
+        }
     }
     
     mysqli_close($link);
