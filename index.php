cbcffff975aa88e704e0f14267fe32303f0e19a6
Jakooth <ivan.k.vladimirov@gmail.com>
2017-05-04 23:40:10 +0300

    Fix collapsed read covers
    It must be hidden, but there was a CSS class priority issues.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <meta content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=yes" name="viewport">
 <?php include ('meta.php'); ?>
 <link rel="stylesheet" type="text/css" href="/jslib/video-js/video-js.css" />
-<link media="only screen and (min-width: 1280px)" rel="stylesheet" type="text/css" href="/assets/forplay.css?v=3.5.4" />
-<link media="only screen and (max-width: 1279px)" rel="stylesheet" type="text/css" href="/assets/forplay-mobile.css?v=3.5.4" />
+<link media="only screen and (min-width: 1280px)" rel="stylesheet" type="text/css" href="/assets/forplay.css?v=3.5.5" />
+<link media="only screen and (max-width: 1279px)" rel="stylesheet" type="text/css" href="/assets/forplay-mobile.css?v=3.5.5" />
 <link rel='stylesheet' type='text/css' href='https://fonts.googleapis.com/css?family=Open+Sans:300,800|Poiret+One&subset=cyrillic,latin' />
 <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 <link rel="icon" href="/favicon.ico" type="image/x-icon">
@@ -74,10 +74,10 @@
 <script src="/controllers/banner.js?v=3.5.2" type="text/javascript">
 	<!--script-->
 </script>
-<script src="/controllers/comment.js?v=3.0.0" type="text/javascript">
+<script src="/controllers/comment.js?v=3.5.1" type="text/javascript">
 	<!--script-->
 </script>
-<script src="/controllers/articles.js?v=3.0.0" type="text/javascript">
+<script src="/controllers/articles.js?v=3.5.1" type="text/javascript">
 	<!--script-->
 </script>
 <script src="/controllers/player.js?v=3.0.0" type="text/javascript">
